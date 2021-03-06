CleanUp, Fixes, See description for details [v0.0.1 - > v0.0.2] 1. Assigning bpy.props [e.g. var = bpy.props.StringProperty] was like thatsee that '=' sign, that was used before 2.80, from 2.80 ':' operatorwas to be used, Well even while using 2.93, i forgot that, there were Warnings Popping, Fixed those -- 2. Addon info doc_url and tracker_url commented out for now -- 3. We used to do crap like sys.path.append, I followed Lucke's auto_load.py [from Animation Nodes 2.2] which was originally written in sometime in 2017, I kept tracking the actual origin and found that in a 2015 Commit, he changed os.walk to pkgutil And before that someTime he also changed the sys.path.append, and started using full absolute paths [relative to addonDirectory] -- 3.1 SO I changed the entire way we used to do import 3.2 It's Kind of stable for now 3.3 Gonna add Some extra features like "Order of Modules, --- order of Packages/Directories, Dependencies" -- 4. Added Licence.txt 5. load_and_reg.py file, plan is to reuse it for any other new addon 6. Changed Indentation to Spaces

nodes/tn_nops.py
@@ -1,28 +1,10 @@
-'''
-Copyright (C) 2021 REYNEP
-parkeramitrakshar@gmail.com
-
-Created by REYNEP
-Thanks to JaquesLucke [Creator of Animation Nodes and Geo Nodes in Blender], 
-Because of the Inspiration and also I mostly Followed His Older Code to Do it Faster [Meaning that, Yes I did copy Parts of His code from 2014-2015]
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
 import bpy
+import os
+import numpy as np
+from pathlib import Path
 from bpy.utils import register_class, unregister_class
 # TODO MAYBE OPTIMIZE
-from tn_utils import *
+from .. tn_utils import *
 
 class AssignActiveObjectToNode(bpy.types.Operator):
 	"""Button that assigns your current active object to this NodeSocket"""
@@ -128,6 +110,30 @@ class AssignActiveObjectToListNode(bpy.types.Operator):
 		node.setObject(obj, self.index)
 		return {'FINISHED'}
 
+#class SaveInitialData(bpy.types.Operator):
+#	""" (Make sure that you have saved your Blend File ina Directory before you Cache, Currently Only Support for this) """
+#	bl_idname = "tn.save_initial_data"
+#	bl_label = "Save Initial Data"
+#
+#	arrayOfData = []
+#	isCacheDir = False
+#
+#	@classmethod
+#	def poll(cls, context):
+#		raise Exception("FOR NOW TN Only supports Caching on Saved BlendFiles (TN Will Create Extra cache Directories)")
+#		return bpy.data.is_saved
+#		
+#	def execute(self, context):
+#		if not isCacheDir:
+#			pathlib.Path.mkdir()
+#
+#		blenDir = os.path.dirname(bpy.data.filepath)
+#		cacheDir = os.path.join(blenDir, "TNCache")
+#		isCacheDir = os.path.exists(cacheDir)
+#
+#		with open()
+#		if len(arrayOfData) > 0:
+#			np.array(arrayOfData).tofile()
 
 
 # Register